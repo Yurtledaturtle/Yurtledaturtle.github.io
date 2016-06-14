0df448ea5236dadb152fa90e1b1d7e49eb5fb32c
Matt Koski <mkoski6@gmail.com>
2016-06-13 22:24:38 -0400

    adds contact page, more css

--- new site/javascripts/script.js
+++ new site/javascripts/script.js
@@ -2,14 +2,60 @@ $(document).ready(function() {
   console.log("Loaded, bro.");
 });
 
+// Modal 1
 // Add an event to OPEN the modal
-$(".open-modal").on("click", function() {
+$("#open-modal1").on("click", function() {
   console.log("You clicked the open button!");
-  $(".modal").toggle();
+  $("#modal1").toggle();
 });
 
 // Add an event to CLOSE the modal
-$(".close-modal").on("click", function() {
+$("#close-modal1").on("click", function() {
   console.log("You clicked the close button.");
-  $(".modal").toggle();
+  $("#modal1").toggle();
 });
+
+
+// Modal 2
+// Add an event to OPEN the modal
+$("#open-modal2").on("click", function() {
+  console.log("You clicked the open button!");
+  $("#modal2").toggle();
+});
+
+// Add an event to CLOSE the modal
+$("#close-modal2").on("click", function() {
+  console.log("You clicked the close button.");
+  $("#modal2").toggle();
+});
+
+
+// Modal 3
+// Add an event to OPEN the modal
+$("#open-modal3").on("click", function() {
+  console.log("You clicked the open button!");
+  $("#modal3").toggle();
+});
+
+// Add an event to CLOSE the modal
+$("#close-modal3").on("click", function() {
+  console.log("You clicked the close button.");
+  $("#modal3").toggle();
+});
+
+
+//Modal 4
+// Add an event to OPEN the modal
+$("#open-modal4").on("click", function() {
+  console.log("You clicked the open button!");
+  $("#modal4").toggle();
+});
+
+// Add an event to CLOSE the modal
+$("#close-modal4").on("click", function() {
+  console.log("You clicked the close button.");
+  $("#modal4").toggle();
+});
+// $(".randquote").on("load", function() {
+//
+// });
